24dfae56965651957b2cfddf3c5e83fc0bcddae7
bnitech <bnitech@naver.com>
2017-07-12 11:37:17 +0900

    main에 있는 sotry 버튼으로 battle Activity 생성 및 호출

--- app/src/main/java/com/bbp/crashtower/MainActivity.java
+++ app/src/main/java/com/bbp/crashtower/MainActivity.java
@@ -1,7 +1,10 @@
 package com.bbp.crashtower;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn_strory).setOnClickListener(
+                new Button.OnClickListener() {
+                    public void onClick(View v) {
+                        Intent intent_battle = new Intent(getApplicationContext(),BattleActivity.class);
+                        startActivity(intent_battle);
+                    }
+                }
+        );
     }
 }
