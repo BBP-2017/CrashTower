b65575f6ed140f396fd697dd7faa1d8fa22253a9
bnitech <bnitech@naver.com>
2017-07-17 12:51:43 +0900

    base activity 생성 및 적용

--- app/src/main/java/com/bbp/crashtower/BaseActivity.java
+++ app/src/main/java/com/bbp/crashtower/BaseActivity.java
@@ -3,13 +3,11 @@ package com.bbp.crashtower;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class BattleActivity extends BaseActivity {
+public class BaseActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_battle);
-
-
+        setContentView(R.layout.activity_base);
     }
 }
