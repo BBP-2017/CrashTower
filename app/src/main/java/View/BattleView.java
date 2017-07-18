6ef3f97df13c531898f77690b5cd0ea32b63a1f4
bnitech <bnitech@naver.com>
2017-07-18 13:30:41 +0900

    main에서 제목표시줄 및 상태표시줄 제거 단말기의 해상도 받아오는 코드 작성

--- app/src/main/java/View/BattleView.java
+++ app/src/main/java/View/BattleView.java
@@ -2,13 +2,21 @@ package View;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.view.Display;
 import android.view.View;
+import android.view.WindowManager;
 
 /**
  * Created by dongbin on 2017-07-18.
  */
 
+
+
 public class BattleView extends View{
+
+    Display display = ((WindowManager) getContext().getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
+    int width = display.getWidth();
+    int height = display.getHeight();
 
     //초기화 영역
     public BattleView(Context context){
