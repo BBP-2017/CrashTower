50fa30e8f2fb4aa60cb23befb68afb66e6d3c4b9
bnitech <bnitech@naver.com>
2017-07-18 13:03:59 +0900

    캐릭터 전투 구현을 위한 BattleView 생성

---
+++ app/src/main/java/View/BattleView.java
@@ -0,0 +1,20 @@
+package View;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.View;
+
+/**
+ * Created by dongbin on 2017-07-18.
+ */
+
+public class BattleView extends View{
+    public BattleView(Context context){
+        super(context);
+
+    }
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+}
