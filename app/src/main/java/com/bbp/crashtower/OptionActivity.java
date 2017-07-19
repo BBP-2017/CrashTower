499e050f17ab1c6a0ecd25027cdb54bf3cfe4155
HANGYU <hans7368@gmail.com>
2017-07-19 17:10:52 +0900

    오류나서 기능을 다 빼버렸습니다

--- app/src/main/java/com/bbp/crashtower/OptionActivity.java
+++ app/src/main/java/com/bbp/crashtower/OptionActivity.java
@@ -7,9 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.Window;
 import android.widget.CheckBox;
-import android.widget.CompoundButton;
 import android.widget.EditText;
-import android.widget.Toast;
 import android.widget.ToggleButton;
 
 /**
@@ -27,70 +25,13 @@ public class OptionActivity extends AppCompatActivity {
 
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_option);
-
         idInput = (EditText)findViewById(R.id.emailInput);
         passwordInput = (EditText)findViewById(R.id.pwInput);
         autoLogin = (CheckBox)findViewById(R.id.checkBox);
-        stb = (ToggleButton)findViewById(R.id.soundBtn);
-
-        // if autoLogin checked, get input
-        if (pref.getBoolean("autoLogin", false)) {
-            idInput.setText(pref.getString("id", ""));
-            passwordInput.setText(pref.getString("pw", ""));
-            autoLogin.setChecked(true);
-            // goto mainActivity
-
-        } else {
-            // if autoLogin unChecked
-            String id = idInput.getText().toString();
-            String password = passwordInput.getText().toString();
-            Boolean validation = loginValidation(id, password);
-
-            if(validation) {
-                Toast.makeText(OptionActivity.this, "Login Success", Toast.LENGTH_LONG).show();
-                if(loginChecked) {
-                    // if autoLogin Checked, save values
-                    editor.putString("id", id);
-                    editor.putString("pw", password);
-                    editor.putBoolean("autoLogin", true);
-                    editor.commit();
-                }
-                // goto mainActivity
-
-            } else {
-                Toast.makeText(OptionActivity.this, "Login Failed", Toast.LENGTH_LONG).show();
-                // goto LoginActivity
-            }
-        }
-
-        // set CheckBoxListener
-        autoLogin.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(isChecked) {
-                    loginChecked = true;
-                } else {
-                    loginChecked = false;
-                    editor.clear();
-                    editor.commit();
-                }
-            }
-        });
-    }
-
-    private boolean loginValidation(String id, String password) {
-        if(pref.getString("id","").equals(id) && pref.getString("pw","").equals(password)) {
-            // login success
-            return true;
-        } else if (pref.getString("id","").equals(null)){
-            // sign in first
-            Toast.makeText(OptionActivity.this, "Please Sign in first", Toast.LENGTH_LONG).show();
-            return false;
-        } else {
-            // login failed
-            return false;
-        }
+        stb = (ToggleButton)this.findViewById(R.id.soundBtn);
     }
 
     public void onClick(View v){
