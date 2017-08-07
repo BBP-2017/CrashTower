5cc6cc0c51491b50f4e21feccafc7f3065e130f0
bnitech <bnitech@naver.com>
2017-08-07 12:44:19 +0900

    inital server class

--- app/src/main/java/com/bbp/crashtower/data/Server.java
+++ app/src/main/java/com/bbp/crashtower/data/Server.java
@@ -1,12 +1,20 @@
 package com.bbp.crashtower.data;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 /**
  * Created by dongbin on 2017-08-07.
  */
 
 public class Server {
 
-    public Server(){
+    //database에 있는 instance를 검색
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
 
-    }
+    //database 내에 data를 저장하고 싶은 위치를 참조.
+    DatabaseReference mutiRef = database.getReference("multi");
+    DatabaseReference singleRef = database.getReference("single");
+
+        
 }
